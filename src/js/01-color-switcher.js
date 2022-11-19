455b631e74cf64d19ad68e158c72457dcf771956
Bellmere <therise00@gmail.com>
2022-11-19 13:46:05 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+const mainEl = document.querySelector('body');
+const controlEl = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+}
+
+controlEl.stopBtn.disabled = true;
+
+let timeId = null;
+
+controlEl.startBtn.addEventListener('click', onClickStart);
+
+controlEl.stopBtn.addEventListener('click', onClickStop);
+
+function onClickStart(e) {
+    if (!e.сurrentTarget) {
+        timeId = setInterval(() => {
+            mainEl.style.background = getRandomHexColor();
+        }, 1000);
+        controlEl.startBtn.disabled = true;
+        controlEl.stopBtn.disabled = false;
+    }
+};
+
+function onClickStop(e) {
+    if (e.currentTarget) {
+        clearInterval(timeId);
+        controlEl.stopBtn.disabled = true;
+        controlEl.startBtn.disabled = false;
+    }
+}
+
+console.log(controlEl);
