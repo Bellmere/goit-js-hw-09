25329cd705572a95e2f75a2522559a7f0dbc0b75
Bellmere <therise00@gmail.com>
2022-11-20 00:00:03 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -16,13 +16,10 @@ Notiflix.Notify.init({
     // ...
   });
 
+  const timerEl = document.querySelector('.timer');
 const inputDateEl = document.querySelector('input#datetime-picker');
 const startBtn = document.querySelector('[data-start]');
 startBtn.disabled = true;
-let timerId = null;
-let eventDateTimer = Number(eventDateTimer);
-console.log(typeof(eventDateTimer));
-
 const eventDate = {
     days: document.querySelector('[data-days]'),
     hours: document.querySelector('[data-hours]'),
@@ -30,53 +27,109 @@ const eventDate = {
     seconds: document.querySelector('[data-seconds]'),
 }
 
-startBtn.addEventListener('click', onTimerId);
-
-function onTimerId(e) {
-  timerId = setInterval(() => {
-    Number(eventDateTimer) - 1000;
-    console.log(this.timerId);
-  },1000);
-}
-
-const date = new Date();
-console.log(date);
-
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-      if (selectedDates[0] < date) {
-        Notiflix.Notify.warning('Please choose a date in the future');
-      }
-      else {
-        startBtn.disabled = false;
-        eventDateTimer =  selectedDates[0] - date;
-        console.log(eventDateTimer);
-      }
-    },
-  };
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    if (selectedDates[0] < new Date()) {
+      Notiflix.Notify.warning('Please choose a date in the future');
+      startBtn.disabled = true;
+    }
+    else {
+      startBtn.disabled = false;
+    }
+  },
+};
 
 flatpickr(inputDateEl, options);
 
 function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+startBtn.addEventListener('click', () => {
+  let timer = setInterval(() => {
+    let countdown = new Date(text.value) - new Date();
+    startBtn.disabled = true;
+    if (countdown >= 0) {
+      let timeObject = convertMs(countdown);
+      eventDate.days.textContent = addLeadingZero(timeObject.days);
+      eventDate.hours.textContent = addLeadingZero(timeObject.hours);
+      eventDate.minutes.textContent = addLeadingZero(timeObject.minutes);
+      eventDate.seconds.textContent = addLeadingZero(timeObject.seconds);
+      if (countdown <= 10000) {
+        timerHtml.style.color = 'tomato';
+      }
+    } else {
+      Notiflix.Notify.success('Countdown finished');
+      timerHtml.style.color = 'black';
+      clearInterval(timer);
+    }
+  }, 1000);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// startBtn.addEventListener('click', e => {
+//     timerId = setInterval(() => {
+//       eventDateTimer -= fixCounter;
+//       console.log(eventDateTimer);
+//     }, 1000);
+// });
+
+// const date = new Date();
+// console.log(date);
+
+// const options = {
+//     enableTime: true,
+//     time_24hr: true,
+//     defaultDate: new Date(),
+//     minuteIncrement: 1,
+//     onClose(selectedDates) {
+//       console.log(selectedDates[0]);
+//       if (selectedDates[0] < date) {
+//         Notiflix.Notify.warning('Please choose a date in the future');
+//       }
+//       else {
+//         startBtn.disabled = false;
+//         eventDateTimer =  Number(selectedDates[0] - date);
+//         console.log(eventDateTimer);
+//       }
+//     },
+//   };
